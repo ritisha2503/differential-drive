23a02e104429cdf6b0924524bdc86ecf1544fca7
ritisha2503 <ritisha2503@gmail.com>
2026-07-02 14:11:55 +0530

    function to update position of robot

--- src/robot.py
+++ src/robot.py
@@ -1,2 +1,12 @@
-def update_pose():
-    pass
+import numpy as np
+
+def update_pose(x, y, theta,
+                vl, vr,
+                wheel_base,
+                dt):
+    v = (vl + vr)/2
+    omega = (vr - vl)/wheel_base
+    x += v * dt * np.cos(theta)
+    y += v * dt * np.sin(theta)
+    theta += omega * dt
+    return x, y, theta
